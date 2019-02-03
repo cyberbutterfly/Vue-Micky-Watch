0ea8664546eefbadf50b787152f6ef4cbdcf307e
goblindaydream <goblin.daydream@gmail.com>
2019-02-03 07:22:24 -0500

    building micky-watch component

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <micky-watch/>
+  </div>
+</template>
+
+<script>
+import MickyWatch from './components/MickyWatch.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MickyWatch
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  background: #000;
+  height:100vh;
+}
+</style>
